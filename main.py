5d3fe87f2003feb1b3c691a6e134be6a506a9833
Feldwor <BoQsc@users.noreply.github.com>
2024-06-01 12:24:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from tkinter import Tk, ttk
 
-app = Tk()
-app.title("Notepad")
-app.maxsize(1000, 400)
+window = Tk()
+window.title("Notepad")
+window.maxsize(1000, 400)
 
-
-frame = ttk.Frame(app, padding=10)
+frame = ttk.Frame(window, padding=10)
 frame.grid()
 ttk.Label(frame, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frame, text="Quit", command=app.destroy).grid(column=1, row=0)
-app.mainloop()
+ttk.Button(frame, text="Quit", command=window.destroy).grid(column=1, row=0)
+window.mainloop()
